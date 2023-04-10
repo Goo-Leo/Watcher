3fc55e0aecee7ff07313a35f8921d26e8cee9016
orangepi <1063376141@qq.com>
2023-04-10 14:34:21 +0800

    MAIN PAGE

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import numpy as np
 import cv2
 
+cap = cv2.VideoCapture(0) #设备号为0
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-def catch_video(name='my_video', video_index=0):
-    # cv2.namedWindow(name)
-    cap = cv2.VideoCapture(video_index)  # 创建摄像头识别类
-    if not cap.isOpened():
-        # 如果没有检测到摄像头，报错
-        raise Exception('Check if the camera is on.')
-    while cap.isOpened():
-        catch, frame = cap.read()  # 读取每一帧图片
-        cv2.imshow(name, frame)  # 在window上显示图片
-        key = cv2.waitKey(10)
-        if key & 0xFF == ord('q'):
-            # 按q退出
-            break
-        if cv2.getWindowProperty(name, cv2.WND_PROP_AUTOSIZE) < 1:
-            # 点x退出
-            break
-    # 释放摄像头
-    cap.release()
-    cv2.destroyAllWindows()
+while(True):
+    if cap.isOpened() == False:
+        print('can not open camera')
+        break
+    ret, frame = cap.read() #读取图像
+    if ret == False: #图像读取失败则直接进入下一次循环
+        continue
+
+	cv2.namedWindow("frame")
+	cv2.imshow('frame', frame)
+
+	mykey = cv2.waitKey(1)
+	#按q退出循环，0xFF是为了排除一些功能键对q的ASCII码的影响
+    if mykey & 0xFF == ord('q'):
+        break
+
+#释放资源
+cap.release()
+cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
-    catch_video()
+
+
